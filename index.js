a737dbaa2e778a0684eef2a7d3d32e7e6abedb89
Firdavs375 <firdavss.3755@gmail.com>
2023-05-03 18:46:40 +0500

    flex

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 let count = document.querySelector(".count")
 let start = document.querySelector(".btn1")
 let reset = document.querySelector(".btn2")
+let audio1 = document.getElementById("audio1")
+let audio2 = document.getElementById("audio2")
+// console.log(audio2);
 
 let a = 0;
 start.onclick = () => {
-    a++
+    a += 1
     count.innerHTML = a;
     if (count.innerHTML == 100) {
-        a=0;
-    }
+        a = 0;
+        audio1.play()
+    } 
+    audio2.play()
 }
 
-reset.onclick = () =>{
-    b = 0;
-    count.innerHTML = b;
+reset.onclick = () => {
+    a = 0;
+    count.innerHTML = a;
+    audio2.play()
 }
-   
